21cc0c27dc77b5ffc857b2c7b21e656390e2ba0b
OseeMukaya <oseemukaya@gmail.com>
2020-08-17 10:24:19 +0200

    correction

--- src/screens/Films/Films.js
+++ src/screens/Films/Films.js
@@ -17,49 +17,54 @@ export default function SingleMoviesPage(props) {
   return (
     <div className="container" style={{marginTop:'80px'}}>
         <div className="row">
-          <div className="col-md-4 card card-body">
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="thumbnail" alt={movie.title} />
+          <div className="col-md-4">
+            <img 
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
+            className="thumbnail" 
+            alt={movie.title} 
+            style={{width:'100%'}}
+            />
           </div>
           <div className="col-md-8">
-            <h2 className="mb-4">{movie.title}</h2>
-            <ul className="list-group">
-            <li className="list-group-item">
+            <h1 className="mb-4">{movie.title}</h1>
+            {/* <ul className="pst-group"> */}
+            <p>
                 <strong>Genre:</strong> 
                 {Array.isArray(movie.genres) &&
                  movie.genres.map(el => <span  key={el.id} className="ml-2">{el.name+','}</span>)}
-              </li>
-              <li className="list-group-item">
+              </p>
+              <p>
                 <strong>Langue originale:</strong> {movie.original_language === "en" ? "Anglais": "Français"}
-              </li>
-              <li className="list-group-item">
+              </p>
+              <p>
                 <strong>Date de sortie:</strong> {movie.release_date}
-              </li>
-              <li className="list-group-item">
+              </p>
+              <p>
                 <strong>Nombre de vote:</strong> {movie.vote_count}
-              </li>
-              <li className="list-group-item">
+              </p>
+              <p>
                 <strong>Moyenne des votes:</strong> {movie.vote_average}
-              </li>
-              <li className="list-group-item">
+              </p>
+              <p>
                 <strong>Popularité:</strong> {movie.popularity}
-              </li>
-              <li className="list-group-item">
-                <strong>Slogan:</strong> {movie.tagline}
-              </li>
-              <li className="list-group-item">
+              </p>
+              <p>
+                <strong>Slogan:</strong> {movie.tagpne}
+              </p>
+              <p>
                 <strong>Sociétés de production:</strong>
                 {Array.isArray(movie.genres) &&
                  movie.production_companies.map(el => <span key={el.id} className="ml-2">{el.name+','}</span>)}
-              </li>
-              <li className="list-group-item">
+              </p>
+              <p>
                 <strong>Pays de production:</strong>
                 {Array.isArray(movie.genres) &&
                  movie.production_countries.map(el => <span  key={el.id} className="ml-2">{el.name+','}</span>)}
-              </li>
-              <li className="list-group-item">
+              </p>
+              <p>
                 <strong>Description:</strong> {movie.overview}
-              </li>
-            </ul>
+              </p>
+            {/* </ul> */}
           </div>
         </div>
       </div>
